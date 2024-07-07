b02ce4b45b2d02295114dd82d63615cefd24ab2a
Vinaybansal8419 <vinaybansal8419@gmail.com>
2024-07-07 09:20:21 +0000

    new

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,6 +1,6 @@
 
 def call(String repoUrl, String destination) {
-    repoUrl = https://github.com/Vinaybansal8419/nginx.git
+    repoUrl = https://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@github.com/Vinaybansal8419/ansible-shared-library.git 
     destination = ./home/vagrant/ansible-shared-library
     sh "git clone ${repoUrl} ${destination}"
 }
